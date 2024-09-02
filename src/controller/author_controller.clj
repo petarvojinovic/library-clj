9138b433fad3676a1ca5d8e81252fb3c7b660388
Petar Vojinović <petar.vojinovic@asseco-see.rs>
2024-09-02 23:08:21 +0200

    Added author validation

--- src/controller/author_controller.clj
+++ src/controller/author_controller.clj
@@ -2,7 +2,8 @@
   (:require [ring.util.response :as response]
             [compojure.core :refer [defroutes GET POST PUT DELETE]]
             [service.author_service :as authors-service]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [validation.author_validation :as validation]))
 
 (defn serialize-to-pretty-json [data]
   (json/generate-string data {:pretty true}))
@@ -20,9 +21,14 @@
 
            (POST "/authors" request
              (let [json-parsed (:body request)]
-               (let [{:keys [name birth_year]} json-parsed]
-                 (authors-service/create-author name birth_year)
-                 (response/response "Author created successfully"))))
+               (let [{:keys [name birth_year]} json-parsed
+                     validation-result (validation/validate-author json-parsed)]
+                 (if (= :ok (:status validation-result))
+                   (do
+                     (authors-service/create-author name birth_year)
+                     (response/response (serialize-to-pretty-json {:message "Author created successfully"})))
+                   (response/status
+                     (response/response (serialize-to-pretty-json validation-result)) 400)))))
 
            (PUT "/authors/:id" request
              (let [json-parsed (:body request)
---
+++ src/validation/author_validation.clj
@@ -0,0 +1,19 @@
+(ns validation.author_validation
+  (:require [clojure.spec.alpha :as s]))
+
+(s/def ::name (s/and string? (complement empty?)))
+(s/def ::birth_year #(re-matches #"\d{4}" (str %)))
+
+(s/def ::author (s/keys :req-un [::name ::birth_year]))
+
+(defn validate-author [author-data]
+  (let [errors (cond-> {}
+                       (not (s/valid? ::name (get author-data :name)))
+                       (assoc :name "Author name must be a non-empty string")
+
+                       (not (s/valid? ::birth_year (get author-data :birth_year)))
+                       (assoc :birth_year "Birth year must be a valid four number integer"))]
+
+    (if (empty? errors)
+      {:status :ok}
+      {:status :error :message errors})))
